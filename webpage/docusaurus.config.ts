72029c7288767a8025ac8ca3ba654557177c8383
Antoniointeli <antonio.teixeira@sou.inteli.edu.br>
2025-02-28 12:39:45 -0300

    doc: teste

---
+++ webpage/docusaurus.config.ts
@@ -0,0 +1,91 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+const config: Config = {
+  title: 'Editais',
+  favicon: 'img/paz.jpg',
+
+  url: 'https://antonio-ang2.github.io',
+  baseUrl: '/editais',
+
+  organizationName: 'antonio-ang2',
+  projectName: 'editais',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'pt-br',
+    locales: ['pt-br'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/antonio-ang2/editais/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
+        },
+        blog: {
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          editUrl:
+            'https://github.com/antonio-ang2/editais/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/paz.jpg',
+    navbar: {
+      title: 'Template docusaurus',
+      logo: {
+        alt: 'Logo Firelink Lib.',
+        src: 'img/paz.jpg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/firelink-library/docusaurus-template',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Firelink Library.
+          Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
